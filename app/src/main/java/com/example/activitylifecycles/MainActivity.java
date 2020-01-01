926877a79580dcbddf3c156f825e9031788d4ec4
Nesar Ahmed <nesar13@hotmail.com>
2019-12-31 19:53:39 -0500

    setOnclickListener and intent on Button

--- app/src/main/java/com/example/activitylifecycles/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycles/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.activitylifecycles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button enterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterButton=findViewById(R.id.enter_button);
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, ShowGuess.class);
+                startActivity(intent);
+
+                // Alternatively: startActivity(new Intent(MainActivity.this,ShowGuess.class));
+                
+
+            }
+        });
     }
 }
