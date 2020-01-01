e4b644c74fb90b09d8fad8de512e0f883d040f49
Nesar Ahmed <nesar13@hotmail.com>
2019-12-31 23:57:37 -0500

    added  features

--- app/src/main/java/com/example/activitylifecycles/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycles/MainActivity.java
@@ -6,25 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button enterButton;
+    private EditText enterGuess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        enterButton=findViewById(R.id.enter_button);
+        enterButton = findViewById(R.id.enter_button);
+        enterGuess= findViewById(R.id.name_text);
 
         enterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, ShowGuess.class);
-                startActivity(intent);
+                String guess=enterGuess.getText().toString().trim();;
+                //allows you to switch between pages
+                Intent intent = new Intent(MainActivity.this, ShowGuess.class);
+                intent.putExtra("Nesar", "Hello there!");
+                startActivity(intent); //order matters
 
                 // Alternatively: startActivity(new Intent(MainActivity.this,ShowGuess.class));
-                
 
             }
         });
--- app/src/main/java/com/example/activitylifecycles/ShowGuess.java
+++ app/src/main/java/com/example/activitylifecycles/ShowGuess.java
@@ -3,12 +3,25 @@ package com.example.activitylifecycles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowGuess extends AppCompatActivity {
+
+    private TextView showGuessText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_guess);
+
+        showGuessText = findViewById(R.id.received_text);
+
+        if ((getIntent().getStringExtra("Nesar") != null)) {
+            String name = getIntent().getStringExtra("Nesar");
+
+            showGuessText.setText(name);
+
+
+        }
     }
 }
